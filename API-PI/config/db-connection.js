196e561b13b63b95402fa4f56369f4503296a736
Pedro Xavier <105009132+OxeMonoYuumi@users.noreply.github.com>
2024-09-18 15:25:42 -0300

    Update db-connection.js

--- API-PI/config/db-connection.js
+++ API-PI/config/db-connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connect = () => {
     mongoose.connect(
-        `mongodb://127.0.0.1:27017/api-theskills`
+        `mongodb+srv://pedroxveiga12:ORLJSXP8kI194260@cluster0.k4l4y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     );
 };
 
